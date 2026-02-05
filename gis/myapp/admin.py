449022480fdbc13c117b5fc3c0d1430f1be3643b
DarkOwl999 <minhc2e2@gmail.com>
2026-02-06 00:04:56 +0700

    Giang sửa "Đăng ký mô hình Apartment trong Django admin"

--- gis/myapp/admin.py
+++ gis/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Apartment
 
-# Register your models here.
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'address', 'lat', 'lng')
+    search_fields = ('name', 'address')
