a35a33f84cdf6996f81a08aaa9b0cd1113a6088d
qntvh2005-blip <qntvh2005@gmail.com>
2026-02-05 23:06:41 +0700

    Hieu sua settings.py them "myapp"
    Changed language code to Vietnamese and updated time zone to Asia/Ho_Chi_Minh.

--- gis/gis/settings.py
+++ gis/gis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "myapp"
 ]
 
 MIDDLEWARE = [
@@ -102,12 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "vi"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Ho_Chi_Minh"
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
