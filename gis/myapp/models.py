eef257b511cbba1451ccb77130e2c0811c735d9c
DarkOwl999 <minhc2e2@gmail.com>
2026-02-05 23:00:58 +0700

    Giang sua o model nha
    alo 1234

--- gis/myapp/models.py
+++ gis/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+# Chon tui con beat
+# hahdai q9roiuqeinf ưepinuomwomiokmgrelmk grlkmj grfljmoknbhmkoln, bbrjnlm,bbfjnml;fnjm l bbmn gd ml cvm l cvmljn;cv j mlk xvjlmn bdfmkj lndfb
+#kjlgonmkjsfdjnokdfmnkojdfgklm
+#kjowsngmiosongsnfjgjknfdgjdfn dfnjkgnjdf jkfdgkjdfk dfng djg dnjgjnkdfg jkdjkg 
+#à3q 32qr ưe rưer ưefwf ừ ưefwe ưerwer sfsdfsd fsd
 
 # Create your models here.
