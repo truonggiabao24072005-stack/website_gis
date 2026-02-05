81d0b97b749a0d51ece6790928f0de967b00144a
Bao Truong <truonggiabao.24072005@gmai.com>
2026-02-05 23:58:29 +0700

    Add templates and static folders

--- gis/myapp/models.py
+++ gis/myapp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-# Chon tui con beat
-# hahdai q9roiuqeinf ưepinuomwomiokmgrelmk grlkmj grfljmoknbhmkoln, bbrjnlm,bbfjnml;fnjm l bbmn gd ml cvm l cvmljn;cv j mlk xvjlmn bdfmkj lndfb
-#kjlgonmkjsfdjnokdfmnkojdfgklm
-#kjowsngmiosongsnfjgjknfdgjdfn dfnjkgnjdf jkfdgkjdfk dfng djg dnjgjnkdfg jkdjkg 
-#à3q 32qr ưe rưer ưefwf ừ ưefwe ưerwer sfsdfsd fsd
 
-# Create your models here.
+class Apartment(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Tên căn hộ")
+    price = models.CharField(max_length=50, verbose_name="Giá tiền (VNĐ)")
+    address = models.CharField(max_length=255, verbose_name="Địa chỉ")
+    desc = models.TextField(verbose_name="Mô tả", blank=True)
+    image = models.ImageField(upload_to='apartments/', verbose_name="Hình ảnh", null=True, blank=True)
+    lat = models.FloatField(verbose_name="Vĩ độ (Latitude)")
+    lng = models.FloatField(verbose_name="Kinh độ (Longitude)")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Căn hộ"            # Tên số ít (khi sửa 1 cái)
+        verbose_name_plural = "Danh sách Căn hộ" # Tên số nhiều (hiện ở menu)
